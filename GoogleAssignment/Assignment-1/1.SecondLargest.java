99a74b6a6a71e228b901153a6260501e4b35fc3c
AnanDEswaran18 <100366969+AnanDEswaran18@users.noreply.github.com>
2022-09-17 06:14:27 +0530

    Update 1.SecondLargest.java

--- GoogleAssignment/Assignment-1/1.SecondLargest.java
+++ GoogleAssignment/Assignment-1/1.SecondLargest.java
@@ -1,1 +1,23 @@
+import java.util.*;
+public class Main {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int Arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            Arr[i]=sc.nextInt();
+        }
+        Arrays.sort(Arr);
+        int num = Arr[n-1];
+        for(int i=n-2;i>=0;i--){
+            if(Arr[i]<num){
+                System.out.print(Arr[i]);
+                break;
+            }
+        }
+    }
+}
 
+//5           ->size
+//1 2 3 4 4   ->arrayElements
+//3           ->secondLargestElement in the array
