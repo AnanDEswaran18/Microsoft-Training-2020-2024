a6cc2bf0b424ce8a02d77660c36a877869a7f844
Anand Eswaran <100366969+AnanDEswaran18@users.noreply.github.com>
2022-10-12 18:53:30 +0530

    Update 21.ConcatenationOfArray.java

--- GoogleAssignment/Assignment-1/21.ConcatenationOfArray.java
+++ GoogleAssignment/Assignment-1/21.ConcatenationOfArray.java
@@ -1,1 +1,15 @@
-
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int len = nums.length;
+        int arr2[] = new int[2*len];
+        for(int i=0;i<len;i++){
+            arr2[i]=nums[i];
+        }
+        int j=len;
+        for(int i=0;i<len;i++){
+            arr2[j]=nums[i];
+            j++;
+        }
+        return arr2;
+    }
+}
